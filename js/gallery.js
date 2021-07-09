5f0a0bc6499c93fa850378d5d32ea672e8ff015f
Yevhen Vasylevytskyi <spirit.ua87@gmail.com>
2021-07-09 22:36:38 +0300

    complete

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,4 @@
 // импорт массива картинок
-
 import galleryItems from './app.js';
 
 // доступ к необходимым эллементам
@@ -27,3 +26,109 @@ const ItemCreateEl = galleryItems.map(({ preview, original, description }) => {
 // добавление в HTML
 ulGalleryEl.insertAdjacentHTML('afterbegin', ItemCreateEl.join(' '));
 
+// добавление слушателей событий
+ulGalleryEl.addEventListener('click', onOpenModalWindow);
+modalWindowEl.addEventListener('click', onControlClick);
+window.addEventListener('keydown', onControlKey);
+
+// открытие модалки
+function onOpenModalWindow(e) {
+    e.preventDefault(); // отмена перехода по ссылке
+
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    };
+
+    modalWindowEl.classList.add('is-open');
+
+    bigImage.src = e.target.dataset.source;
+    bigImage.alt = e.target.alt;
+};
+
+// управление кликами на модалке
+function onControlClick(e) {
+    if (e.target.dataset.action === 'prev-lightbox') {
+        prevImg();
+    };
+
+    if (e.target.dataset.action === 'next-lightbox') {
+        nextImg();
+    };
+
+    if (e.target.dataset.action !== 'close-lightbox' && e.target.nodeName !== 'DIV') {
+        return;
+    };
+
+    closeModalWindow();
+};
+
+//// управление стрелками при открытой модалке
+function onControlKey(e) {
+    if (modalWindowEl.classList.value.includes('is-open')) {
+        if (e.keyCode === 27) {
+            //console.log('esc');
+            closeModalWindow();
+        };
+        
+        if (e.keyCode === 37) {
+            //console.log('влево');
+            prevImg();
+        };
+
+        if (e.keyCode === 39) {
+            //console.log('вправо');
+            nextImg();
+        };
+    };    
+};
+
+// поиск элемента в списке, который отображается на модалке в текущий момент
+function findCurrentElement() {
+    let index;
+
+    for (let i = 0; i < ulGalleryEl.children.length; i++) {
+        if (ulGalleryEl.children[i].children[0].href === bigImage.src) {
+            index = i;
+            break;
+        }
+    }
+
+    return index;
+};
+
+// предыдущая картинка на модалке
+function prevImg() {
+    const index = findCurrentElement();
+
+    if (index === 0) {
+        bigImage.src = ulGalleryEl.children[ulGalleryEl.children.length-1].children[0].children[0].dataset.source;
+        bigImage.alt = ulGalleryEl.children[ulGalleryEl.children.length-1].children[0].children[0].alt;
+    }
+    
+    else {
+        bigImage.src = ulGalleryEl.children[index - 1].children[0].children[0].dataset.source;
+        bigImage.alt = ulGalleryEl.children[index - 1].children[0].children[0].alt;
+    };
+};
+
+// следующая картинка на модалке
+function nextImg() {
+    const index = findCurrentElement();
+
+    if (index === ulGalleryEl.children.length-1) {
+        bigImage.src = ulGalleryEl.children[0].children[0].children[0].dataset.source;
+        bigImage.alt = ulGalleryEl.children[0].children[0].children[0].alt;
+    }
+
+    else {
+        bigImage.src = ulGalleryEl.children[index + 1].children[0].children[0].dataset.source;
+        bigImage.alt = ulGalleryEl.children[index + 1].children[0].children[0].alt;
+    };
+};
+
+// закрытие модалки
+function closeModalWindow() {
+    modalWindowEl.classList.remove('is-open');
+    bigImage.src = '';
+    bigImage.alt = '';
+};
